27339ab1f8f4551311f8f2f61285fe0590b33179
Tony Wang <tony94042@gmail.com>
2022-12-13 12:09:26 +1100

    Added Working Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning Database'
+Movie.destroy_all
+
+url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=e21f347015063e52941c0f886ea87a98&language=en-US&page=1'
+user_serialized = URI.open(url).read
+user = JSON.parse(user_serialized)
+movies = user['results']
+
+movies.each do |movie|
+  title = movie['original_title']
+  overview = movie['overview']
+  votes =  movie['vote_average']
+  poster = movie['backdrop_path']
+
+  Movie.create(title: title, overview: overview, poster_url: "https://image.tmdb.org/t/p/w500#{poster}", rating: votes)
+end
+
+List.create!(name: "Classic")
+List.create!(name: "Action")
+
+Bookmark.create!(comment: 'Classic Movie', movie_id: 1, list_id: 1)
+Bookmark.create!(comment: 'Childhood Favourite', movie_id: 2, list_id: 2)
+
+puts 'Seeds Created'
