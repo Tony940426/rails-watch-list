e26cfb93febd9f30f0333288fbf614d10689b503
Tony Wang <tony94042@gmail.com>
2022-12-15 12:07:35 +1100

    All CRUD Action Now Functiona;

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,17 +1,10 @@
 class BookmarksController < ApplicationController
   before_action :set_list, only: [:new, :create]
 
-  # GET /bookmarks/new
   def new
     @bookmark = Bookmark.new
   end
 
-  # GET /bookmarks/1/edit
-  def edit
-
-  end
-
-  # POST /bookmarks or /bookmarks.json
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
@@ -22,19 +15,11 @@ class BookmarksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /bookmarks/1 or /bookmarks/1.json
-  def update
-
-  end
-
-  # DELETE /bookmarks/1 or /bookmarks/1.json
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to list_path(@bookmark.list), status: :see_other
   end
-
-
 
   def set_list
     @list = List.find(params[:list_id])
