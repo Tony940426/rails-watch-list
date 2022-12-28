d7c1df18a21fd27fc044043317e27e91f6e6a554
Tony Wang <tony94042@gmail.com>
2022-12-28 13:26:11 +1100

    Started to add AJAX Movie Search Function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,25 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 window.bootstrap = bootstrap
+
+console.log("Connected!")
+
+const fetchMovies = (movie) => {
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=e21f347015063e52941c0f886ea87a98&language=en-US&query=${movie}&page=1&include_adult=false`)
+  .then(response =>response.json())
+  .then(data => {
+    const movie = data.results[0]
+    console.log(movie.original_title)
+    console.log(movie.overview)
+    console.log(movie.vote_average)
+    console.log(movie.poster_path)
+  });
+}
+
+const form = document.getElementById("search-form")
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = event.currentTarget.querySelector(".form-control");
+  console.log(input.value)
+  fetchMovies(input.value)
+});
