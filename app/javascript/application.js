c8fd69cd778c6b0de87daa39364f5a14ef46ac13
Tony Wang <tony94042@gmail.com>
2022-12-29 14:51:37 +1100

    Search

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,23 +5,33 @@ import * as bootstrap from "bootstrap"
 window.bootstrap = bootstrap
 
 console.log("Connected!")
+const list = document.querySelector('#results');
+
+const insertMovies = (data) => {
+  const movies = data.results
+  movies.forEach((movie) => {
+    console.log(movie.original_title)
+    console.log(movie.overview)
+    console.log(movie.poster_path)
+
+    const movieTag = `<li>
+    <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="" />
+    <p>${movie.original_title}</p>
+    </li>`;
+    list.insertAdjacentHTML('beforeend', movieTag);
+  });
+}
 
 const fetchMovies = (movie) => {
   fetch(`https://api.themoviedb.org/3/search/movie?api_key=e21f347015063e52941c0f886ea87a98&language=en-US&query=${movie}&page=1&include_adult=false`)
   .then(response =>response.json())
-  .then(data => {
-    const movie = data.results[0]
-    console.log(movie.original_title)
-    console.log(movie.overview)
-    console.log(movie.vote_average)
-    console.log(movie.poster_path)
-  });
-}
+  .then(insertMovies);
+};
 
 const form = document.getElementById("search-form")
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  list.innerHTML = '';
   const input = event.currentTarget.querySelector(".form-control");
-  console.log(input.value)
   fetchMovies(input.value)
 });
