8eb593f2ef06e22c63430b86288083708b7f5e2d
Tony Wang <tony94042@gmail.com>
2022-12-29 13:43:14 +1100

    Trying to Add Search Functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,12 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    if params[:query].present?
+      sql_query = 'title ILIKE :query OR overview ILIKE :query'
+      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
   end
 
   # GET /movies/1 or /movies/1.json
